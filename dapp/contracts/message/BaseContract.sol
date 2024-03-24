a33878ed8432e9df048f3919eb635a465ba685be
yurakas97 <123570342+yurakas97@users.noreply.github.com>
2024-03-25 00:28:35 +0200

    Update BaseContract.sol

--- dapp/contracts/message/BaseContract.sol
+++ dapp/contracts/message/BaseContract.sol
@@ -2,103 +2,128 @@
 
 pragma solidity ^0.8.9;
 
+import "../base/CustomChanIbcApp.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-import "../base/CustomChanIbcApp.sol";
-
-contract BaseContract is ERC721, CustomChanIbcApp {
+/**
+ * @title nft-mint
+ * @dev Implements minting process,
+ * and ability to send cross-chain instruction to mint NFT on counterparty
+ */
+contract OptContract is ERC721, CustomChanIbcApp, ERC721Burnable {
+    using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
 
-    struct Joke {
-        string text;
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    // Base URI
+    string private _baseURIextended;
+    uint256 public tokenID;
+
+    enum IbcPacketStatus {
+        UNSENT,
+        SENT,
+        ACKED,
+        TIMEOUT
     }
-    mapping(uint256 => Joke) public jokes;
 
-    string baseURI;
-    string jokee;
+    address public chairperson;
 
-    event MintedOnRecv(bytes32 channelId, uint64 sequence, uint256 voteNFTId);
+    event AckNFTMint(
+        bytes32 channelId,
+        uint sequence,
+        address indexed voter,
+        uint voteNFTid
+    );
 
+    /**
+     * @dev Create a new ballot to choose one of 'proposalNames' and make it IBC enabled to send proof of Vote to counterparty
+     * @param _dispatcher vIBC dispatcher contract
+     * @param proposalNames names of proposals
+     */
     constructor(
         IbcDispatcher _dispatcher,
-        string memory _baseURI
+        bytes32[] memory proposalNames
     ) CustomChanIbcApp(_dispatcher) ERC721("JokeNFT", "JNFT") {
-        baseURI = _baseURI;
-    }
-
-    function mint(
-        address recipient
-    )
-        internal
-        returns (
-            //string memory jokeText
-            uint256
-        )
-    {
-        currentTokenId.increment();
-        uint256 tokenId = currentTokenId.current();
-        _safeMint(recipient, tokenId);
-        //jokes[tokenId] = Joke(jokeText);
-        return tokenId;
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view virtual override returns (string memory) {
-        // require(
-        //     _exists(tokenId),
-        //     "ERC721Metadata: URI query for nonexistent token"
-        // );
-
-        return string(abi.encodePacked(jokee));
-    }
-
-    function updateBaseURI(string memory _newBaseURI) public {
-        baseURI = _newBaseURI;
+        chairperson = msg.sender;
     }
 
     // IBC methods
 
-    // This contract only receives packets from the IBC dispatcher
+    /**
+     * @dev Sends a packet with a greeting message over a specified channel.
+     * @param channelId The ID of the channel to send the packet to.
+     * @param timeoutSeconds The timeout in seconds (relative).
+     * @param voterAddress the address of the voter
+     * @param str recipient the address on the destination (Base) that will have NFT minted
+     */
+    function sendPacket(
+        bytes32 channelId,
+        uint64 timeoutSeconds,
+        address voterAddress,
+        string memory str
+    ) external {
+        string memory jokeFromId = str;
+        address owner = voterAddress;
+        bytes memory payload = abi.encode(owner, jokeFromId);
+
+        uint64 timeoutTimestamp = uint64(
+            (block.timestamp + timeoutSeconds) * 1000000000
+        );
+
+        dispatcher.sendPacket(channelId, payload, timeoutTimestamp);
+    }
 
     function onRecvPacket(
-        IbcPacket memory packet
-    ) external override onlyIbcDispatcher returns (AckPacket memory ackPacket) {
-        recvedPackets.push(packet);
+        IbcPacket memory
+    )
+        external
+        view
+        override
+        onlyIbcDispatcher
+        returns (AckPacket memory ackPacket)
+    {
+        require(false, "This function should not be called");
 
-        // Decode the packet data
-        (
-            address decodedVoter,
-            string memory str //string memory decodedJoke
-        ) = abi.decode(packet.data, (address, string));
-
-        // Mint the NFT
-        jokee = str;
-        uint256 voteNFTid = mint(decodedVoter);
-        emit MintedOnRecv(packet.dest.channelId, packet.sequence, voteNFTid);
-
-        // Encode the ack data
-        bytes memory ackData = abi.encode(decodedVoter, voteNFTid);
-
-        return AckPacket(true, ackData);
+        return
+            AckPacket(
+                true,
+                abi.encode("Error: This function should not be called")
+            );
     }
 
     function onAcknowledgementPacket(
-        IbcPacket calldata,
-        AckPacket calldata
-    ) external view override onlyIbcDispatcher {
-        require(
-            false,
-            "This contract should never receive an acknowledgement packet"
+        IbcPacket calldata packet,
+        AckPacket calldata ack
+    ) external override onlyIbcDispatcher {
+        ackPackets.push(ack);
+
+        // decode the ack data, find the address of the voter the packet belongs to and set ibcNFTMinted true
+        (address voterAddress, uint256 voteNFTid) = abi.decode(
+            ack.data,
+            (address, uint256)
+        );
+
+        emit AckNFTMint(
+            packet.src.channelId,
+            packet.sequence,
+            voterAddress,
+            voteNFTid
         );
     }
 
     function onTimeoutPacket(
-        IbcPacket calldata
-    ) external view override onlyIbcDispatcher {
-        require(false, "This contract should never receive a timeout packet");
+        IbcPacket calldata packet
+    ) external override onlyIbcDispatcher {
+        timeoutPackets.push(packet);
+        // do logic
     }
 }
